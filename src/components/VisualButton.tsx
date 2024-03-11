d2464e0b3545fef2cf087d403e9d9bc15b6431d1
chosule <89799325+chosule@users.noreply.github.com>
2024-03-11 23:19:24 +0900

    ✨ Add: VisualButton circle 도형 추가

--- src/components/VisualButton.tsx
+++ src/components/VisualButton.tsx
@@ -3,6 +3,7 @@
 import useSelectedTabStore from "@/store/useStore";
 import Button from "./commons/Button";
 import Link from "next/link";
+import { FaCircle } from "react-icons/fa";
 
 export default function VisualButton() {
   const { selectedTab } = useSelectedTabStore();
@@ -11,15 +12,17 @@ export default function VisualButton() {
     <>
       {selectedTab.id === 1 ? (
         <div className="flex gap-5 justify-end">
-          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] ">
+          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] flex items-center justify-center gap-1">
+            <FaCircle />
             <Link href="#">Google Play</Link>
           </Button>
-          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] ">
+          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] flex items-center justify-center gap-1">
+            <FaCircle />
             <Link href="#">App Store</Link>
           </Button>
         </div>
       ) : (
-        <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] ">
+        <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px]">
           <Link href="#">로그인</Link>
         </Button>
       )}
