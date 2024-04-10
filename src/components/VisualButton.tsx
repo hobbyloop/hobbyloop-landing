ce2e4094991f7a1bed0755f022802d57105ce5d0
chosule <89799325+chosule@users.noreply.github.com>
2024-04-11 00:22:43 +0900

    💄 Add: 미디어쿼리 수정

--- src/components/VisualButton.tsx
+++ src/components/VisualButton.tsx
@@ -11,12 +11,12 @@ export default function VisualButton() {
   return (
     <>
       {selectedTab.id === 1 ? (
-        <div className="flex gap-5 justify-end">
-          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] flex items-center justify-center gap-1">
+        <div className="flex gap-5 justify-end order-3 md:order-2 col-start-1">
+          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] justify-self-end lg:mt-[-54px] flex items-center justify-center gap-1">
             <FaCircle />
             <Link href="#">Google Play</Link>
           </Button>
-          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] order-3 md:order-2 col-start-1 justify-self-end lg:mt-[-54px] flex items-center justify-center gap-1">
+          <Button className="text-neutral-80 bg-neutral-500 w-full md:w-[147px] h-[44px] justify-self-end lg:mt-[-54px] flex items-center justify-center gap-1">
             <FaCircle />
             <Link href="#">App Store</Link>
           </Button>
